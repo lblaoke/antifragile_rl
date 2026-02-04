ecec63526b1c07fca89d47d7d5b539af52002ce0
Bolian Li <lblaoke@foxmail.com>
2026-02-03 19:20:19 -0500

    after deletion

---
+++ interactive_debug_gpu4.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+sinteractive \
+    --job-name=debug \
+    --partition=ai \
+    --account=ruqiz \
+    --qos=preemptible \
+    --nodes=1 \
+    --ntasks-per-node=4 \
+    --gres=gpu:4 \
+    --cpus-per-task=28 \
+    --time=0-01:00:00
---
+++ scripts/launch.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+sbatch scripts/grpo_nsr_trainer/qwen3-4b-base_numina_math_grpo_nsr_n8_once1.slurm
+sbatch scripts/grpo_nsr_trainer/qwen3-4b-base_numina_math_grpo_nsr_n8_once10.slurm
